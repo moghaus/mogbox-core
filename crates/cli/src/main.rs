e1347d7607e802d8af6d2da503757ad52b10968c
zjcrumpton <zjcrumpton@gmail.com>
2025-10-31 00:10:46 -0600

    parse cmd line args

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct CmdArg {
+    // Which operation to perform on the file
+    cmd: String,
+    // The path to the file we want to operate on
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    todo!()
+    let args = CmdArg::parse();
+    println!("Command: {:?} File Path: {:?}", args.cmd, args.path)
 }
